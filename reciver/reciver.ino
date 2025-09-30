2e22e473271a4a853817651ae6eb1ae0964799d6
elyasafDana <elidnana@gmail.com>
2025-09-30 19:20:42 +0300

    Add files via upload

---
+++ reciver/reciver.ino
@@ -0,0 +1,62 @@
+#include <LowPower.h>
+
+#define clock_in 2
+#define data_in 3
+
+
+unsigned long startTime;  // משתנה לשמירת זמן תחילת הפעולה
+unsigned long elapsedTime; 
+
+void onInterapte();
+volatile  char c;
+String data="";
+int count=0;
+volatile  bool gotNewString=false;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(clock_in,INPUT);
+pinMode(data_in,INPUT);
+Serial.begin(2000000);
+
+attachInterrupt(digitalPinToInterrupt(clock_in),onInterapte , RISING  );
+}
+
+void loop() {
+  
+  //put your main code here, to run repeatedly:
+  if(gotNewString){
+   Serial.print("the word is: ");
+  Serial.print(data);  // הדפסת התו
+  Serial.println();
+    gotNewString=false;
+  }
+  delayMicroseconds(1);
+
+}
+void onInterapte(){
+  //Serial.println("on interpat");
+  delayMicroseconds(10);
+  if(digitalRead(data_in)==1){
+    c=(c<<1) | 1;
+    Serial.print("1");
+  }
+  else{
+    c=(c<<1) | 0;
+    Serial.print("0");
+  }
+
+count++;
+if(count%8==0){
+  data=data+c;
+  Serial.println();
+  //c='';
+  //Serial.print(c); 
+  gotNewString=true;
+  //Serial.println("done");
+}
+
+
+
+
+
+}
---
+++ sender/sender.ino
@@ -0,0 +1,61 @@
+#define clock_out 2
+#define data_out 3
+
+
+
+
+
+String input = "";
+void sendOther(char c);
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(clock_out,OUTPUT);
+pinMode(data_out,OUTPUT);
+ digitalWrite(clock_out,LOW);
+Serial.begin(2000000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("please enter your word:");
+while(Serial.available()==0){
+
+}
+if(Serial.available()>0){
+   input=Serial.readStringUntil('\n');
+   
+
+}
+  Serial.println(input);
+
+for(int i=0;i<input.length();i++){
+  sendOther(input.charAt(i));
+  delayMicroseconds(50);
+}
+
+}
+
+
+void sendOther(char c){
+for(int i=7;i>=0;i--){
+  digitalWrite(clock_out,HIGH);
+  delayMicroseconds(1);
+
+  if( (c>>i)&1==1){
+    digitalWrite(data_out,HIGH);
+    Serial.print("1");
+
+  } 
+  else{
+    digitalWrite(data_out,LOW);
+    Serial.print("0");
+  } 
+  delayMicroseconds(50);
+  digitalWrite(clock_out,LOW);
+  delayMicroseconds(1000);
+}
+    Serial.println();
+
+
+}
